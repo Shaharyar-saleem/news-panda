349e2fecee7ee1c2092d378ff20b157ea3cbb340
shaharyar saleem <shaharyarsaleem71@gmail.com>
2022-03-22 10:54:48 +0100

    feat: data fetched from API

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,16 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import NewsItem from './NewsItem';
 
+
 export class News extends Component {
+    // 4053b8b400004c86982aebd728731683  //News API key
+
+    constructor() {
+        super()
+        this.state = {
+           articles: this.articles,
+           loading: false,
+        }
+    }
+
+    async componentDidMount() {
+        const url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4053b8b400004c86982aebd728731683";
+        let data = await fetch(url)
+        let fetchedData = await data.json()
+        console.log("cmd data:",fetchedData.articles)
+        this.setState({articles: fetchedData.articles})
+        console.log("state value", this.state.articles)
+    }
   render() {
+    const customContainer = {
+        width: '80%',
+        margin: '0 auto',
+    }
     return (
-        <>
-      <div>News Block</div>
-      <NewsItem />
-      <NewsItem />
-      <NewsItem />
-      <NewsItem />
-      </>
+        <div className="my-5" style={customContainer}>
+            <h2 className="text-center">News Panda - latest News</h2>
+            <div className="row">
+                {
+                this.state.articles ? this.state.articles.map((article, key)=>{
+                   return <div className="col-md-3 mt-4" key={key}>
+                            <NewsItem title={article.title} description={article.description} imageUrl={article.urlToImage}/>
+                          </div>
+                }) : ''
+            }
+            </div>
+      </div>
     )
   }
 }
--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let {title, description, imageUrl} = this.props;
     return (
-      <div>
-        news item component
-      </div>
+      <>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="#" className="btn btn-primary">
+                News Detail
+            </a>
+          </div>
+        </div>
+      </>
     );
   }
 }
