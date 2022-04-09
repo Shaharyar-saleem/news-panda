0759f8b5bcdbcf49c456b379a96729f7f230f000
shaharyar saleem <shaharyarsaleem71@gmail.com>
2022-04-09 14:32:22 +0200

    update(Loading): updated to functional component

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import loading from "../img/loading.gif";
 
-export default class Loading extends Component {
-  render() {
+const Loading = (props) => {
     return (
       <div className="text-center">
-        <img className="my-5" src={loading} alt={loading} width={this.props.width} height={this.props.height}/>
+        <img className="my-5" src={loading} alt={loading} width={props.width} height={props.height}/>
       </div>
     );
-  }
 }
+
+export default Loading;
